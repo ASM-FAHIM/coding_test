7a2455cce31ece2370e7a17e9d68eac4e52f1750
Abu Saleh Mohammad Fahim <36271104+ASM-FAHIM@users.noreply.github.com>
2023-02-07 12:42:39 +0600

    Update dimensions.dart

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -4,50 +4,21 @@ class Dimensions {
   static double screenHeight = Get.context!.height;
   static double screenWidth = Get.context!.width;
 
-  //for slider big container
-  static double pageView = screenHeight / 2.5;
-  static double pageViewContainer = screenHeight / 3.63;
-  static double pageViewTextContainer = screenHeight / 6.66;
-
   //dynamic height padding and margin(top/bottom/height/width)
   static double height10 = screenHeight / 84.4;
-  static double height15 = screenHeight / 56.27;
-  static double height20 = screenHeight / 42.2;
-  static double height35 = screenHeight / 22.85;
-  static double height45 = screenHeight / 17.77;
+  static double height40 = screenHeight / 20.0;
+  static double height268 = screenHeight / 2.98;
+  static double height345 = screenHeight / 2.28;
+  static double width370 = screenHeight / 2.16;
+  static double width350 = screenHeight / 2.28;
 
   //dynamic width padding and margin(left/right)
   static double width10 = screenHeight / 84.4;
   static double width15 = screenHeight / 56.27;
   static double width20 = screenHeight / 42.2;
-  static double width35 = screenHeight / 22.85;
-  static double width45 = screenHeight / 17.77;
 
   //font size
   static double fontBigS16 = screenHeight / 51.75;
   static double fontBigS20 = screenHeight / 42.2;
   static double fontBigS26 = screenHeight / 30.76;
-
-  //for border radius
-  static double radius15 = screenHeight / 53.33;
-  static double radius20 = screenHeight / 42.2;
-  static double radius30 = screenHeight / 28.13;
-
-  //for icon size
-  static double iconSize25 = screenHeight / 32.00;
-  static double iconSize16 = screenHeight / 52.75;
-  static double iconSize40 = screenHeight / 20.00;
-
-  //list view size
-  static double listViewImgSize = screenWidth / 3.25;
-  static double listViewTextContSIze = screenWidth / 3.9;
-
-  //popular food page
-  static double popularFoodImgSize = screenHeight / 2.5;
-
-  //bottom height
-  static double bottomHeightBar = screenHeight / 6.66;
-
-  //splash screen dimensions
-  static double splashImg = screenHeight / 2.2;
 }
